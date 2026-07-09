b3aaca77ae839c9303e652224cd11e5ba9096872
ranganiharsh5185 <ranganiharsh5185@gmail.com>
2026-07-09 21:10:51 +0530

    Refactor linked list implementation with deletion

--- LAB12/2.c
+++ LAB12/2.c
@@ -3,16 +3,14 @@
 struct node
 {
     int info;
-    struct node *link;
+    struct node *rptr;
+    struct node *lptr;
 };
 struct node *FIRST = NULL;
-struct node *SECOND = NULL;
-
-void halves();
 void insert_at_last();
-void display2();
-void display1();
-int count();
+void delete_alternate();
+
+void display();
 
 void main()
 {
@@ -20,7 +18,7 @@ void main()
     while (c != 0)
     {
         printf("0.to exit\n");
-        printf("1.to insert node\n");
+        printf("1.to insert node:");
         scanf("%d", &c);
         if (c == 0)
         {
@@ -28,106 +26,69 @@ void main()
         }
         insert_at_last();
     }
-    halves();
-    display1();
-    display2();
-}
-void halves()
-{
-    if (FIRST == NULL){
-
-        return;
-    }
-
-    int n = count();
-    struct node *save = FIRST;
-    struct node *save2 = FIRST;
-
-    while (save2->link != FIRST)
-    {
-        save2 = save2->link;
-    }
-
-    for (int i = 1; i < (n + 1) / 2; i++)
-    {
-        save = save->link;
-    }
-
-    SECOND = save->link;
-    save->link = FIRST;
-    save2->link = SECOND;
+    delete_alternate();
+    display();
 }
 void insert_at_last()
 {
     struct node *save, *newnode;
+
     newnode = (struct node *)malloc(sizeof(struct node));
 
-    printf("Enter the data: ");
+    printf("Enter data: ");
     scanf("%d", &newnode->info);
+
+    newnode->rptr = NULL;
 
     if (FIRST == NULL)
     {
         FIRST = newnode;
-        newnode->link = FIRST;
+        newnode->lptr = NULL;
+        return;
     }
-    else
-    {
-        save = FIRST;
 
-        while (save->link != FIRST)
-        {
-            save = save->link;
-        }
-        newnode->link = FIRST;
-        save->link = newnode;
+    save = FIRST;
+
+    while (save->rptr != NULL)
+    {
+        save = save->rptr;
     }
+
+    save->rptr = newnode;
+    newnode->lptr = save;
 }
-int count()
+void display()
 {
     struct node *save;
     save = FIRST;
-    int count = 0;
-    do
+    while (save != NULL)
     {
-        count++;
-        save = save->link;
-    } while (save != FIRST);
-
-    return count;
+        printf("%d<->", save->info);
+        save = save->rptr;
+    }
+    printf("\n");
 }
-void display1()
+void delete_alternate()
 {
-    struct node *save;
-    save = FIRST;
+    struct node *save, *pred, *succ;
+
     if (FIRST == NULL)
+        return;
+
+    save = FIRST;
+
+    while (save != NULL && save->rptr != NULL)
     {
-        printf("List is empty\n");
+        pred = save;
+        succ = save->rptr;
+
+        pred->rptr = succ->rptr;
+
+        if (succ->rptr != NULL)
+            succ->rptr->lptr = pred;
+
+        free(succ);
+
+        save = pred->rptr;
     }
-    else
-    {
-        do
-        {
-            printf("%d ", save->info);
-            save = save->link;
-        } while (save != FIRST);
-    }
-    printf("\n");
-}
-void display2()
-{
-    struct node *save;
-    save = SECOND;
-    if (SECOND == NULL)
-    {
-        printf("List is empty\n");
-    }
-    else
-    {
-        do
-        {
-            printf("%d ", save->info);
-            save = save->link;
-        } while (save != SECOND);
-    }
-    printf("\n");
 }
